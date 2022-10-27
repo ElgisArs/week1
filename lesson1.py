5d3e4cdeb8f6e1d2c0949fd055a6dc0bf39193f7
ElgisArs <elgiselvis@gmail.com>
2022-10-27 09:33:06 +0600

    add week1

---
+++ lesson1.py
@@ -0,0 +1,9 @@
+#str1 = "Elvis"
+#print(str1 * 3)
+#print(str1)
+#print(len)
+#txt = "Hello bootcamp"
+#make_new_txt = txt.maketrans("b" "t")
+#print(txt)
+#print(txt.translate(make_new_txt))
+ # print(2)
